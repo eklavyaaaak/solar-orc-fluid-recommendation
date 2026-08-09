b05e1d3233df23d26c9ec650be8c6fb49b8d38c8
Eklavya Kumar <eklavya.kr7374@gmail.com>
2026-08-09 10:44:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
 from flask import Flask, request, jsonify, render_template
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/simulate", methods=["POST"])
+def simulate():
+    data = request.get_json()
+
+    solar = float(data.get("solar", 0))
+    temperature = float(data.get("temperature", 25))
+
+    if solar >= 800 and temperature >= 30:
+        fluid = "Toluene"
+    elif solar >= 500:
+        fluid = "R245fa"
+    else:
+        fluid = "R134a"
+
+    return jsonify({
+        "recommended_fluid": fluid,
+        "solar_radiation": solar,
+        "temperature": temperature
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
